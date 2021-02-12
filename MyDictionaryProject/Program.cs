1fea9a8eeacb8e78fecce39f16fd6fa6f7986007
Abdullah Göçeren <abdullahgoceren@gmail.com>
2021-02-12 15:36:30 +0300

    Add project files.

---
+++ MyDictionaryProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionaryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Ayakkabı","Bot");
+            myDictionary.Add("Takı", "Bileklik");
+            myDictionary.List();
+
+            Console.WriteLine("\nTanımlanan Katagori Sayısı : " + myDictionary.Count());
+        }
+    }
+}
